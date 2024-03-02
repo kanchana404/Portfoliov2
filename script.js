8e49da879718b4dbd1dcf4f644e96619c5e98b1c
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-03-02 23:10:54 +0530

    done

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-new fullpage ("#fullpage",{
+new fullpage("#fullpage", {
     autoScrolling: true,
-    navigation: true,
+    navigation: false,
+
+});
+
+function animateText() {
+    gsap.from(".animated-text", {
+        opacity: 0, // Start from opacity 0
+        duration: 5, // Animation duration
+        delay: 0.5, // Delay before animation starts
+        ease: "power2.out" // Easing function
+    })
+    gsap.from("#contact", {
+        opacity: 0, // Start from opacity 0
+        duration: 5, // Animation duration
+        delay: 1, // Delay before animation starts
+        ease: "power2.out"
+    })
+    ;
+}
+
+// Call the animation function when the document is ready
+document.addEventListener("DOMContentLoaded", function () {
+    animateText();
 });
